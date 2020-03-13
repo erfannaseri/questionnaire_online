769d296ac607a81a41bb694a2db4564ac0f34244
Erfan-Naseri <er9.naseri@gmail.com>
2020-03-13 22:31:16 +0330

    git add controll named questioncontroller and define new route to create qusetions

--- routes/web.php
+++ routes/web.php
@@ -15,3 +15,7 @@ Route::get('questionnaire/create','QuestionnaireController@create')->name('quest
 Route::post('questionnaires','QuestionnaireController@store')->name('questionnaire.store');
 Route::get('/{user}/questionnaires','QuestionnaireController@index')->name('questionnaire.index');
 Route::get('questionnaires/{questionnaire}','QuestionnaireController@show')->name('questionnaire.show');
+
+
+Route::get('questionnaire/{questionnaire}/question/create','QuestionController@create')
+    ->name('question.create')->middleware('auth');

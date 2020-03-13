3d0373126e5e8812e4c3c4deaa2e92da7657bc99
Erfan-Naseri <er9.naseri@gmail.com>
2020-03-13 22:39:33 +0330

    add link add question to back\questionnaire\index

--- resources/views/back/questionnaire/index.blade.php
+++ resources/views/back/questionnaire/index.blade.php
@@ -15,6 +15,7 @@
                                 <th>کاربرد</th>
                                 <th>طراح</th>
                                 <th>جزییات</th>
+                                <th>----</th>
                             </tr>
                             </thead>
                             <tbody>
@@ -24,6 +25,7 @@
                                 <td>{{$questionnaire->purpose}}</td>
                                 <td>{{$questionnaire->user->name}}</td>
                                 <td><a href="{{route('questionnaire.show',$questionnaire->title)}}" class="btn btn-success btn-outline-primary">جزییات</a></td>
+                                <td><a class="btn btn-info btn-outline-primary" href="{{route('question.create',$questionnaire->title)}}">طراحی سوالات</a></td>
                             </tr>
                             @empty
                                 <p class="alert alert-info">هیچ پرسشنامه ای ثبت نشده است</p>

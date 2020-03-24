faff3061d1bbad369f181036f69a4db076b3fbb7
Erfan-Naseri <er9.naseri@gmail.com>
2020-03-24 22:10:15 +0430

    create relation in Question model and that method named resposes

--- app/Question.php
+++ app/Question.php
@@ -18,4 +18,9 @@ class Question extends Model
     {
         return $this->hasMany(Answer::class);
     }
+
+    public function responses()
+    {
+        return $this->hasMany(SurveyResponse::class);
+    }
 }

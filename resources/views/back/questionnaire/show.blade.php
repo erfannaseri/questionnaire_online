57418c77f777aa8daf024b056cae9249170ee4dd
Erfan-Naseri <er9.naseri@gmail.com>
2020-03-24 22:03:01 +0430

    create counter for pepole select each option

--- resources/views/back/questionnaire/show.blade.php
+++ resources/views/back/questionnaire/show.blade.php
@@ -31,7 +31,7 @@
                                         @foreach($question->answers as $key=>$answer)
                                             <li dir="rtl" class="list-group-item">
                                                 <div>{{ $key +1 }} - {{ $answer->answer }}</div>
-
+                                                <div>{{$answer->responses()->count()}}</div>
                                             </li>
                                         @endforeach
                                     </ul>

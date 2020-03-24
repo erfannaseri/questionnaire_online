7114790ba2f37b6c56db8dbe64708acccfb59f59
Erfan-Naseri <er9.naseri@gmail.com>
2020-03-24 12:29:45 +0430

    define fillable for SurveyResponse  model

--- app/SurveyResponse.php
+++ app/SurveyResponse.php
@@ -8,6 +8,10 @@ class SurveyResponse extends Model
 {
     protected $guarded=[];
 
+    protected $fillable =[
+        'survey_id','answer_id','question_id'
+    ];
+
     public function survey()
     {
         return $this->belongsTo(Survey::class);

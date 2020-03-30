937acb681d90ee1d5f8df065996cbc0f33d878f5
Erfan-Naseri <er9.naseri@gmail.com>
2020-03-30 15:25:47 +0430

    add index method to front/QuestionnaireController

--- app/Http/Controllers/front/QuestionnaireController.php
+++ app/Http/Controllers/front/QuestionnaireController.php
@@ -3,9 +3,15 @@
 namespace App\Http\Controllers\front;
 
 use App\Http\Controllers\Controller;
+use App\Questionnaire;
 use Illuminate\Http\Request;
 
 class QuestionnaireController extends Controller
 {
-    //
+
+    public function index()
+    {
+        $chart=Questionnaire::all();
+        return view('front.chart',compact('chart'));
+    }
 }

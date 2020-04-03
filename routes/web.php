ecc0b0099fe8aea17e33182986614033fade0022
Erfan-Naseri <er9.naseri@gmail.com>
2020-04-03 15:38:28 +0430

    remove testing route in web.php in line 37

--- routes/web.php
+++ routes/web.php
@@ -33,5 +33,3 @@ Route::group(['middleware'=>'auth','prefix'=>'questionnaires'],function (){
 
 Route::get('surveys/{id}-{slug}','SurveyController@show')->middleware('auth');
 Route::post('surveys/{id}-{slug}','SurveyController@store')->middleware('auth');
-
-Route::view('test','front/index');

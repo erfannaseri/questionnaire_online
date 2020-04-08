4d92e19ba09acc96f3084096a0fab0a536e4ebbf
Erfan-Naseri <er9.naseri@gmail.com>
2020-04-08 18:55:59 +0430

    add  view for create courses in back/panels/admin/course/

---
+++ resources/views/back/panels/admin/course/create-course.blade.php
@@ -0,0 +1,60 @@
+@extends('layouts.app')
+
+@section('content')
+    <div class="container">
+        <div class="row justify-content-center">
+            @include('back.panels.admin.sideBar')
+            <div class="col-md-8">
+                <div class="card">
+                    <div class="card-header">{{ __('ثبت درس جدید') }}</div>
+
+                    <div class="card-body">
+                        <form method="POST" action="{{ route('course.store') }}">
+                            @csrf
+
+                            <div class="form-group row">
+                                <label for="name" class="col-md-4 col-form-label text-md-right">{{ __('عنوان') }}</label>
+
+                                <div class="col-md-6">
+                                    <input id="title" type="text" class="form-control @error('title') is-invalid @enderror" name="title" value="{{ old('title') }}" required autocomplete="name" autofocus>
+
+                                    @error('title')
+                                    <span class="invalid-feedback" role="alert">
+                                        <strong>{{ $message }}</strong>
+                                    </span>
+                                    @enderror
+                                </div>
+                            </div>
+
+                            <div class="form-group row">
+                                <label for="email" class="col-md-4 col-form-label text-md-right">{{ __('پایه تحصیلی') }}</label>
+
+                                <div class="col-md-6">
+                                    <input id="grade" type="text" class="form-control @error('grade') is-invalid @enderror" name="grade" value="{{ old('grade') }}" required autocomplete="email">
+
+                                    @error('grade')
+                                    <span class="invalid-feedback" role="alert">
+                                        <strong>{{ $message }}</strong>
+                                    </span>
+                                    @enderror
+                                </div>
+                            </div>
+
+
+
+
+                            <div class="form-group row mb-0">
+                                <div class="col-md-6 offset-md-4">
+                                    <button type="submit" class="btn btn-primary">
+                                        {{ __('ثبت') }}
+                                    </button>
+                                </div>
+                            </div>
+                        </form>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+@endsection
+

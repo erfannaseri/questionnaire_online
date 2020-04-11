a426a7c970f699af4a0d0f1b40fee117535fed34
Erfan-Naseri <er9.naseri@gmail.com>
2020-04-11 13:19:01 +0430

    fix checkUser,checkAdmin,checkStudent middlewares for deny accessing to other panels

--- app/Http/Middleware/CheckStudent.php
+++ app/Http/Middleware/CheckStudent.php
@@ -5,7 +5,7 @@ namespace App\Http\Middleware;
 use Closure;
 use App\Services\Roles\Enums\RoleTypes;
 use Illuminate\Support\Facades\Auth;
-
+use Illuminate\Support\Facades\Request;
 class CheckStudent
 {
     /**
@@ -17,7 +17,7 @@ class CheckStudent
      */
     public function handle($request, Closure $next)
     {
-        if (Auth::check() && Auth::user()->role == RoleTypes::STUDENT) {
+        if (Auth::check() && Auth::user()->role == RoleTypes::STUDENT &&  Auth::user()->username  == Request::segment(2)) {
             return $next($request);
         }
         return  redirect(route('error-404'));
